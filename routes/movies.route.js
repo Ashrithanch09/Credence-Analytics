033a1a193ccd488bbbff107d5b745070fdea961c
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-05-17 16:54:45 +0530

    Add files via upload

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,1 +1,22 @@
+const router = require("express").Router();
+const formidableMiddleware = require("express-formidable");
 
+const {
+  getAllMoviesController,
+  createMovieController,
+  getSingleMoviesController,
+  deleteMovieController,
+  updateMovieController,
+} = require("../controllers/movies.controller");
+
+router.post("/", formidableMiddleware(), createMovieController);
+
+router.get("/", getAllMoviesController);
+
+router.get("/:id", getSingleMoviesController);
+
+router.put("/:id", updateMovieController);
+
+router.delete("/:id", deleteMovieController);
+
+module.exports = router;
